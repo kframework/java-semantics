780686423a839ba8dff69738c5f03e013bc1760e
Denis Bogdanas <denis.bogdanas@gmail.com>
2012-05-05 06:37:47 +0000

    1. Migrated java semantics to latest version of K. All tests pass. 2. Added a workaround for a bug in xorBool. 3. Added one more smoke test for read.

---
+++ programs/01_smoke_tests/read.java
@@ -0,0 +1,14 @@
+public class main {
+
+	public static void main(string[] args) {
+    print("Insert a number: ");
+    int a = read();
+    print("You inserted: ", a, "\n");
+    print("Done!", "\n");
+	}
+}
+
+// (for input 15)
+
+// Insert a number: You inserted: 15
+// Done!
