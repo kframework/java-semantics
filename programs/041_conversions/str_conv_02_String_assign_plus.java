0feda5e6a0aca134fc45b002462244d9777cea52
Denis Bogdanas <denis.bogdanas@gmail.com>
2012-06-06 11:15:51 +0000

    Implemented += operator for left-hand operand String.

---
+++ programs/041_conversions/str_conv_02_String_assign_plus.java
@@ -0,0 +1,17 @@
+/*
+String += cases:
+  - string += string
+  - string += int
+  - array[string] += string
+*/
+public class str_conv_02_String_assign_plus {
+  public static void main(String[] args) {
+    String s = "ab";
+    System.out.println(s+="cde");
+    System.out.println(s+=12);
+    String[] vs = new String[1];
+    vs[0] = s;
+    System.out.println(vs[0]+="cde");
+    System.out.println("Done!");
+  }
+}
---
+++ programs/041_conversions/str_conv_03_Object_assign_plus.java
@@ -0,0 +1,17 @@
+/*
+String += cases:
+  - object += string
+  - array[object] += string
+*/
+public class str_conv_03_Object_assign_plus {
+  public static void main(String[] args) {
+    Object o = new RuntimeException("re-");
+    Object o2 = o;
+    System.out.println(""+(o2+="zz"));
+
+    Object[] vo = new Object[1];
+    vo[0] = o;
+    System.out.println(vo[0]+="cde");
+    System.out.println("Done!");
+  }
+}
