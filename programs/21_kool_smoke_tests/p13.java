048f5a9d107c785ac9b9eabeac99fc6b9af8dc82
Denis Bogdanas <denis.bogdanas@gmail.com>
2012-06-06 17:42:57 +0000

    Refactored subtype, cast, instanceof implementations.

--- programs/21_kool_smoke_tests/p13.java
+++ programs/21_kool_smoke_tests/p13.java
@@ -1,32 +1,50 @@
-class c {
-  c() {  }
-  void test() {
-    System.out.print(-1);
-    if (this instanceof c1) System.out.print(1);
-    if (this instanceof c2) System.out.print(2);
-    if (this instanceof c3) System.out.print(3);
-    System.out.println("");
+class a {
+  int x;
+  a() {
+    x=7;
+  }
+  int m0() {
+    return x;
   }
 }
 
-class c1 extends c {
-  c1() { super(); }
-}
-
-class c2 extends c {
-  c2() { super(); }
-}
-
-class c3 extends c {
-  c3() { super(); }
+class b extends a {
+  boolean x ;
+  b() {
+    super();
+    x=true;
+  }
+  int m1() {
+    return super.x;
+  }
+  int m2() {
+    a o = this;
+    return o.x;            // implicit casting
+    // the field x is the one in the type a of o, that is, 7
+  }
+  int m3() {
+    return ((a) this).x;  // explicit casting
+    //"cast object to class" casts "object" to "class"
+  }
+  int m4() {
+    return aux1(this);
+  }
+  int aux1(a o) {   // implicit casting, again
+    return o.x;
+  }
+  boolean m5() {
+    return aux2(this);
+  }
+  boolean aux2(a o) {       // implicit casting, again
+    return ((b) o).x;     // but then explicit casting back to b
+  }
 }
 
 class main {
   main(String[] args) {
-    (new c()).test();
-    (new c1()).test();
-    (new c2()).test();
-    (new c3()).test();
+    b o = new b();
+    if (o.m5()) System.out.println(""+o.m0()+ o.m1()+ o.m2()+ o.m3()+ o.m4());
+    else System.out.println("m5 not returning true");
     System.out.println("Done!");
   }
 }
