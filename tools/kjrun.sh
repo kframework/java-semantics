2f3096a5533496e989448b6fddf0227ce026f1bf
Denis Bogdanas <denis.bogdanas@gmail.com>
2012-10-23 14:30:05 +0000

    Minor refactoring of the tool.

--- tools/kjrun.sh
+++ tools/kjrun.sh
@@ -13,4 +13,4 @@ TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # The last argument is not used, we put it here just to overcome file presence checking of krun
 # We cannot put $javaFile directly because krun expects only files,
 # and $javaFile may also be a dir
-krun --parser="aux-kjprep.sh $javaFile" $TOOLS_DIR/kjrun.sh
+krun --parser=aux-kjprep.sh $javaFile
