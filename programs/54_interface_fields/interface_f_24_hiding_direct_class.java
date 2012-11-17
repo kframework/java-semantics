39a68625ac75f0c66a4756291acd90cb1dca98f8
Denis Bogdanas <denis.bogdanas@gmail.com>
2012-11-17 02:57:31 +0000

    Finished implementing fields in interfaces. All tests pass. Some more tests are still planned.

--- programs/54_interface_fields/interface_f_24_hiding_direct_class.java
+++ programs/54_interface_fields/interface_f_24_hiding_direct_class.java
@@ -1,26 +1,51 @@
 /*
 Fields hiding involving an interface:
-  A{v} < I1{v}
+  A{static v, u} < I1{v,u}
 
-  Test using static unqualified and class-qualified expression.
+  Test using all possible expressions.
 */
 
 public class interface_f_24_hiding_direct_class {
   public static void main(String[] args) {
-    System.out.println(I1.v + " " + A.v + " " + A.getV());
+    A a = new A();
+    System.out.print("v instance context: ");
+    System.out.println(a.v + " " + ((I1)a).v + " " + a.getV() + " " + a.getThisV());
+    System.out.print("v static context  : ");
+    System.out.println(A.v + " " + I1.v + " " + A.staticGetV());
+    System.out.print("u instance context: ");
+    System.out.println(a.u + " " + ((I1)a).u + " " + a.getU() + " " + a.getThisU());
+
     System.out.println("Done!");
   }
 }
 
 interface I1 {
-  int v = 11;
+  String v = "I1.v";
+  String u = "I1.u";
 }
 
 class A implements I1 {
 
-  static int v = 12;
+  public static String v = "A.v";
+  public String u = "A.u";
 
-  static int getV() {
+  String getV() {
     return v;
+  }
+
+  String getThisV() {
+    return this.v;
+  }
+
+  static String staticGetV() {
+    return v;
+  }
+
+  String getU() {
+    return u;
+  }
+
+  String getThisU() {
+    return this.u;
   }
 }
--- programs/54_interface_fields/interface_f_25_hiding_indirect_class.java
+++ programs/54_interface_fields/interface_f_25_hiding_indirect_class.java
@@ -1,28 +1,62 @@
 /*
 Fields hiding involving an interface:
-  B < A{v} < I1{v}
+  B < A{static v, u} < I1{v,u}
 
-  Test using static unqualified and class-qualified expression.
+  Test using all possible expressions, from the context B.
 */
 
 public class interface_f_25_hiding_indirect_class {
   public static void main(String[] args) {
-    System.out.println(I1.v + " " + B.v + " " + B.getV());
+    B b = new B();
+    System.out.print("v instance context: ");
+    System.out.println(b.v + " " + b.getV() + " " + b.getThisV() + " " + b.getSuperV());
+    System.out.print("v static context  : ");
+    System.out.println(B.v + " " + B.staticGetV());
+    System.out.print("u instance context: ");
+    System.out.println(b.u + " " + b.getU() + " " + b.getThisU() + " " + b.getSuperU());
+
     System.out.println("Done!");
   }
 }
 
 interface I1 {
-  int v = 11;
+  String v = "I1.v";
+  String u = "I1.u";
 }
 
 class A implements I1 {
-  static int v = 12;
+
+  public static String v = "A.v";
+  public String u = "A.u";
 }
 
 class B extends A {
 
-  static int getV() {
+  String getV() {
     return v;
+  }
+
+  String getThisV() {
+    return this.v;
+  }
+
+  String getSuperV() {
+    return super.v;
+  }
+
+  static String staticGetV() {
+    return v;
+  }
+
+  String getU() {
+    return u;
+  }
+
+  String getThisU() {
+    return this.u;
+  }
+
+  String getSuperU() {
+    return super.u;
   }
 }
