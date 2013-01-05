e0616612aa0d05e3315f70b4534cdd5b0a61517d
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-01-05 18:10:53 +0000

    Tools: 1. Created one more script: jdktest.sh that executes only the generator,   and writes in test-results.xml the result returned by JDK java.   Useful to verify the tests themselves.
    2. Bugfix. jdk-run.sh don't delete the temporary directories if the test contains inner directories,
      or even in any case when packages are used.

--- tools/jdk-run.sh
+++ tools/jdk-run.sh
@@ -7,5 +7,25 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-aux-jdk-run.sh $1
-find -type f -name "*.class" -exec rm -f {} \;
+javaFile=$1
+
+mainFile=`basename $javaFile`
+mainClass=`echo "$mainFile" | cut -d'.' -f1`
+
+if [ -f $javaFile ];
+then
+  #if the file starts with a package declaration, add package to the main class name
+  read -r IN < $javaFile
+  PACKAGE_STR=${IN:0:7}
+  if [ "$PACKAGE_STR" == "package" ];
+    then mainClass="${IN:8:${#IN}-9}.$mainClass"
+  fi
+  JAVAC_ARG=$javaFile
+else
+  JAVAC_ARG=$(find -P $javaFile -name "*.java" -type f)
+fi
+
+mkdir .jdk-run
+javac -d .jdk-run $JAVAC_ARG
+java -ea -cp .jdk-run $mainClass
+rm -r -f .jdk-run
