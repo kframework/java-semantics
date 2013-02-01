dea2765d7f4ef491a67207e72b85aa5c6b8b27f8
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-02-01 14:44:55 +0000

    static inner classes: Implemented access modes for the members of inner classes.

--- programs/63_inner_cl_static/inner_st_104_access_modes.java/P1Test.java
+++ programs/63_inner_cl_static/inner_st_104_access_modes.java/P1Test.java
@@ -5,4 +5,10 @@ public class P1Test {
   public String test() {
     return "p1.P1Test.test(): " + new p1.A.B().f((byte)0);
   }
+
+  public static class D {
+    public String test() {
+      return "p1.P1Test.D.test(): " + new p1.A.B().f((byte)0);
+    }
+  }
 }
