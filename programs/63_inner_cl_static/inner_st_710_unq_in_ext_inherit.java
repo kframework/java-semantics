8eb222554c17ce622550b01d8d63a76f65b6a36c
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-02-04 18:33:53 +0000

    static inner classes: Implemented references to inner classes by simple name inside extends clauses of other inner classes.

--- programs/63_inner_cl_static/inner_st_710_unq_in_ext_inherit.java
+++ programs/63_inner_cl_static/inner_st_710_unq_in_ext_inherit.java
@@ -4,7 +4,7 @@ Inner class in extends clause, it's base class is inherited
   Classes A, A.InnerA, B < A, B.InnerB < InnerA (referred by simple name).
 */
 
-public class inner_st_710_unq_inext_inherit {
+public class inner_st_710_unq_in_ext_inherit {
 
   public static void main(String[] args) {
     System.out.println(new B.InnerB());
