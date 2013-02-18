73b6a3ea1ea88539ca2135b0e2a1c2fa44b30b1f
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-02-18 19:59:09 +0000

    tools: General cleanup. 1. Deleted tools that are no longer used. 2. Added the tool aux-kjtest.sh with arguments threads and clean. 3. Added a timeout of 30s to kjrun.sh

--- tools/t1-kjtest.sh
+++ tools/t1-kjtest.sh
@@ -2,39 +2,6 @@
 
 # if no arguments are provided - running the whole test suite.
 # if several arguments are provided as directories - running
-# tests inside that directories. Uses only one thread.
+# tests inside that directories.
 
-TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [ $# == 0 ] || [ $1 != "-clean" ];
-  then
-    CLEAN=false
-    STARTARG=1
-  else
-    CLEAN=true
-    STARTARG=2
-fi
-
-if [ $# -lt $STARTARG ]; then
-    $TOOLS_DIR/t1-kjtest.sh ${@} $TOOLS_DIR/../programs/
-    exit
-fi
-
-START=$(date +%s)
-PRETTYARGS=$(find ${@:STARTARG} -maxdepth 0)
-echo "Running tests for"
-echo "$PRETTYARGS"
-echo
-
-# We don't use external script for deleting temp dir,
-# since it have the same performance as standart delete from java.
-java -cp $TOOLS_DIR/test-runner.jar ro.uaic.javasemantics.tools.Main \
-  -gen $TOOLS_DIR/aux-jdk-run.sh -run $TOOLS_DIR/aux-kjrun.sh \
-  -taskExt java -threads 1 -timeout 30 -testsuiteName java-semantics \
-  -clean $CLEAN ${@:STARTARG}
-
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo
-echo "It took $DIFF seconds"
-
+aux-kjtest.sh -threads 1 -clean false ${@}
