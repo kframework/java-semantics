07d463742214c7ea4d802e32808ec8ea9ddc2c27
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-04-30 19:25:57 +0000

    elaborate blocks phase: Elaborated cast, variable declarations in for initializer and in catch clause.

---
+++ tools/kjrawrun.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Run a java program with krun.
+
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    echo "Usage: `basename $0` <javaFile>"
+    echo "or"
+    echo "Usage: `basename $0` <javaFile> --debug"
+    exit 1
+fi
+
+JAVA_FILE=$1
+BASE_JAVA_FILE=`basename $JAVA_FILE`
+MAIN_CLASS=`echo "$BASE_JAVA_FILE" | cut -d'.' -f1`
+TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# The last argument is not used, we put it here just to overcome file presence checking of krun
+# We cannot put $JAVA_FILE directly because krun expects only files,
+# and $JAVA_FILE may also be a dir
+if [ $# == 1 ];
+then
+    time timeout 30 krun --parser=aux-kjprep.sh --output-mode=raw -cMainClass="ListItem(\"$MAIN_CLASS\")" $JAVA_FILE
+else
+    krun --parser=aux-kjprep.sh --output-mode=raw -cMainClass="ListItem(\"$MAIN_CLASS\")" --debug $JAVA_FILE
+fi
+
