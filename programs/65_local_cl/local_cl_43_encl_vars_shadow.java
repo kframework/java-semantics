eb80376fb739e67661b2bb55cb679701021c1d3d
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-08-05 15:27:42 +0000

    local classes: Implemented correct priority when enclosing local vars and fields are interleaved on multiple levels.

--- programs/65_local_cl/local_cl_43_encl_vars_shadow.java
+++ programs/65_local_cl/local_cl_43_encl_vars_shadow.java
@@ -4,7 +4,7 @@ Local class accessing a local var that shadows a field of the enclosing class.
 
 public class local_cl_43_encl_vars_shadow {
   public static void main(String[] args) {
-    System.out.println(new O().createLB());
+    System.out.println(new O().createLocal());
     System.out.println("Done!");
   }
 }
@@ -13,14 +13,14 @@ class O {
 
   int a = 100;
 
-  Object createLB() {
+  Object createLocal() {
 
     final int a = 1, b = 20;
 
-    class LB {
-      public String toString() {return "O.meth().LB: a="+a + " ,b="+b+", O.this.a="+O.this.a;}
+    class Local {
+      public String toString() {return "O.createLocal().Local: a="+a + " ,b="+b+", O.this.a="+O.this.a;}
     }
 
-    return new LB();
+    return new Local();
   }
 }
