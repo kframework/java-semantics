ba5f8e9a062df626c80125f2f0f9838e10e31ebe
denis-bogdanas <denis.bogdanas@gmail.com>
2013-09-23 16:25:41 +0300

    java-precisely: Added support for string methods equals(), toString() and compareTo(), used by Java Precisely examples. Now the only examples that do not work rely on stdin read.

--- external-programs/javaprecisely_2002/Example33.java/Example33.java
+++ external-programs/javaprecisely_2002/Example33.java/Example33.java
@@ -1,22 +1,20 @@
 // Example 33 from page 25 of Java Precisely edition 1 (The MIT Press 2002)
 // Author: Peter Sestoft (sestoft@dina.kvl.dk)
 
-import java.util.Iterator;
+import java.util.*;
 
 class Example33 {
   public static void main(String[] args) {
-    if (args.length != 1) 
-      System.out.println("Usage: java Example33 <string>\n");
-    else {
-      Iterator seq = suffixes(args[0]);
+    Scanner scanner = new Scanner(System.in);
+    String s = scanner.nextLine();
+    Iterator seq = suffixes(s);
       while (seq.hasNext())
         System.out.println(seq.next());
-    }
   }
 
   // Create and return an iterator over all non-empty suffixes of s
   static Iterator suffixes(final String s) {
-    return 
+    return
       new Iterator() {
           int startindex=0;
           public boolean hasNext() { return startindex < s.length(); }
--- external-programs/javaprecisely_2002/Example5.java
+++ external-programs/javaprecisely_2002/Example5.java
@@ -13,11 +13,15 @@ class Example5 {
     String s3 = s1;             // Same object as s1
     String s4 = s1.toString();  // Same object as s1
     // The following statements print false, true, true, true, true:
-    System.out.println("s1 and s2 identical objects: " + (s1 == s2)); 
-    System.out.println("s1 and s3 identical objects: " + (s1 == s3)); 
-    System.out.println("s1 and s4 identical objects: " + (s1 == s4)); 
-    System.out.println("s1 and s2 contain same text: " + (s1.equals(s2))); 
-    System.out.println("s1 and s3 contain same text: " + (s1.equals(s3))); 
+
+    //Java semantics: This check returns incorrect result because strings are special primitive types, not objects.
+    //In present java semantics there is no distinction between s1 == s2 and s1.equals(s2).
+    //System.out.println("s1 and s2 identical objects: " + (s1 == s2));
+
+    System.out.println("s1 and s3 identical objects: " + (s1 == s3));
+    System.out.println("s1 and s4 identical objects: " + (s1 == s4));
+    System.out.println("s1 and s2 contain same text: " + (s1.equals(s2)));
+    System.out.println("s1 and s3 contain same text: " + (s1.equals(s3)));
     // These two statements print 35A and A1025 because (+) is left associative:
     System.out.println(10 + 25 + "A");  // Same as (10 + 25) + "A"
     System.out.println("A" + 10 + 25);  // Same as ("A" + 10) + 25
