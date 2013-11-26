bfb5c3b90a593597641aed33c7ae0a5048c65ef5
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-11-26 17:12:11 +0200

    tools: Replaced 4 kjkompile scripts with one having options. Also a nice help added.

--- tools/kjkompile-latex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# kompile a latex file from java definition. To be run from semantics dir.
-
-TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-KOMPILE_CMD=$(cross-k.sh kompile)
-
-$KOMPILE_CMD -v --backend pdf --doc-style "style=math" java
---
+++ tools/kjkompile.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# kompile java into one of five possible outputs:
+# exec - regular kompilation, for execution
+# strictness - kompilation for strictness checking
+# threading - kompilation for threading checking
+# latex - compile into latex
+# pdf - compile into pdf
+
+if [ $# -gt 1 ]; then
+    echo "Usage: `basename $0` or `basename $0` <exec|strictness|threading|latex|pdf>"
+    exit
+fi
+
+TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KOMPILE_CMD=$(cross-k.sh kompile)
+
+if [ $# -eq 1 ];
+  then OPTION=$1
+  else OPTION=exec
+fi
+
+case "$OPTION" in
+"exec")
+    $KOMPILE_CMD -v java
+    ;;
+"strictness")
+    $KOMPILE_CMD -v -transition "transition-strictness" java
+    ;;
+"threading")
+    $KOMPILE_CMD -v -transition "transition-threading" java
+    ;;
+"latex")
+    $KOMPILE_CMD -v --backend latex --doc-style "style=math" java
+    ;;
+"pdf")
+    $KOMPILE_CMD -v --backend pdf --doc-style "style=math" java
+    ;;
+*)
+    echo "Invalid option: $OPTION"
+    echo "Usage: `basename $0` or `basename $0` <exec|strictness|threading|latex|pdf>"
+    exit 1
+    ;;
+esac
