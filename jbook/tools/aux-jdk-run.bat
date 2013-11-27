98ffd141e86a7278b480d85cd97f9dedcbf8f2ee
Denis Bogdanas <denis.bogdanas@gmail.com>
2013-11-27 13:31:38 +0200

    tools: Related to the previous commit. Aggregated tools.

--- jbook/tools/aux-jdk-run.bat
+++ jbook/tools/aux-jdk-run.bat
@@ -1,7 +1,7 @@
 @echo off
-REM Runs the given program with the script aux-jdk-run.sh
+REM Runs the given program with the script jdk-run.sh
 
 REM the fully qualified path to the current directory
 SET JBOOK_TOOLS_DIR=%~dp0
 
-%JBOOK_TOOLS_DIR%\bash-run.bat %JBOOK_TOOLS_DIR%\..\..\tools\aux-jdk-run.sh %1
+%JBOOK_TOOLS_DIR%\bash-run.bat %JBOOK_TOOLS_DIR%\..\..\tools\jdk-run.sh %1
