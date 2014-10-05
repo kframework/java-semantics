91c456745b4d5a8f523735213d0f003e1dbddab1
Denis Bogdanas <denis.bogdanas@gmail.com>
2014-10-05 13:52:05 +0300

    #KJ-234 Submit K-Java for POPL artifact evaluation. Updated the semantics to the same version as the prepared VM. Also some more corrections were made in examples and ltl-syntax. This is the same version of K-Java as the VM prepared for evaluation.

---
+++ invalid/invalidTypeConv.java
@@ -0,0 +1,10 @@
+public class invalidTypeConv {
+
+  public static void main(String[] args) {
+    int a = "x";
+    System.out.println("Done!");
+  }
+}
+
+// Hello World!
+// Done!
---
+++ invalid/undefinedClass.java
@@ -0,0 +1,10 @@
+public class undefinedClass {
+
+  public static void main(String[] args) {
+    A a;
+    System.out.println("Done!");
+  }
+}
+
+// Hello World!
+// Done!
