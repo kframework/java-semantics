661899fbca5373025bb2b21b5b4780587ab51f75
laurayuwen <laurayuwen@gmail.com>
2014-12-20 23:03:18 -0600

    added one more macro on EnumConstList

--- tools/kjparse.sh
+++ tools/kjparse.sh
@@ -9,6 +9,6 @@ fi
 
 JAVA_FILE=$(cross-path-native.sh $1)
 TOOLS_DIR="$( cd "$( dirname "$0" )" && pwd )"
-#PARSER_JAR=$(cross-path-native.sh ${TOOLS_DIR}/../parser/JavaParser.jar)
-PARSER_JAR=$(cross-path-native.sh ${TOOLS_DIR}/../parser/javafrontparser.jar)
+PARSER_JAR=$(cross-path-native.sh ${TOOLS_DIR}/../parser/JavaParser.jar)
+#PARSER_JAR=$(cross-path-native.sh ${TOOLS_DIR}/../parser/javafrontparser.jar)
 java -jar ${PARSER_JAR} ${JAVA_FILE}
