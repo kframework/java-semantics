ddc13a890913ede72402de1a4e3ca0433c5965ae
xiaohe27 <hexiao2@illinois.edu>
2015-04-05 21:59:53 -0500

    prep-he.sh works

--- tools/prep-he.sh
+++ tools/prep-he.sh
@@ -2,7 +2,7 @@ OUT="$1.pkast"
 
 TMP="tmp1.txt"
 
-krun $1 --directory "../src/prep" --parser "kj-parse-aggreg.sh" --output kast | sed "s/\`'KListWrap/\`'ListWrap/g" > $TMP
+krun $1 --directory "../src/prep" --parser "kj-parse-aggreg.sh" --output kast > $TMP
 
 tr -d ' ' <$TMP >$OUT
 tr -d '\t' <$OUT >$TMP
@@ -12,7 +12,7 @@ tr -d '\r' <$OUT >$TMP
 sed -r 's/.*<program>//g' < $TMP > $OUT
 sed -r 's/<\/program>.*//g' < $OUT > $TMP
 
-mv $TMP $OUT
+sed "s/\`'KListWrap/\`'ListWrap/g" < $TMP > $OUT
 
 ClsName=$(echo $1 | tr -d ".java")
 krun $OUT --directory "../src/exec" -cMainClass="ListItem(\"$ClsName\")" -cDissolveAllExceptOut="true" --parser "cat" --output none
