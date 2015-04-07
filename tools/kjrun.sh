490ad377af6227e224f8d309e9e0a696e535ec63
xiaohe27 <hexiao2@illinois.edu>
2015-04-06 21:23:34 -0500

    gen the intermediate output at cur dir

--- tools/kjrun.sh
+++ tools/kjrun.sh
@@ -1,8 +1,17 @@
-OUT="$1.pkast"
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-TMP="tmp1.txt"
+CurDir=$(pwd)
 
-krun $1 --directory "../src/prep" --parser "kj-parse-aggreg.sh" --output kast > $OUT
+Input=$(basename $1)
+OUT="$CurDir/$Input.pkast"
+
+TMP="$CurDir/tmp.txt"
+
+krun $1 --directory "$SCRIPTPATH/../src/prep" --parser "kj-parse-aggreg.sh" --output kast > $OUT
 
 sed 's/\s*\([[:graph:]].*[[:graph:]]\)\s*/\1/g' < $OUT | tr -d '\n' > $TMP
 
@@ -11,5 +20,7 @@ sed -r 's/<\/program>.*//g' < $OUT > $TMP
 
 sed "s/\`'KListWrap/\`'ListWrap/g" < $TMP > $OUT
 
-ClsName=$(echo $1 | sed -r 's/\.java$//g')
-krun $OUT --directory "../src/exec" -cMainClass="ListItem(\"$ClsName\")" -cDissolveAllExceptOut="true" --parser "cat" --output none
+rm $TMP
+
+ClsName=$(echo $Input | sed -r 's/\.java$//g')
+krun $OUT --directory "$SCRIPTPATH/../src/exec" -cMainClass="ListItem(\"$ClsName\")" -cDissolveAllExceptOut="true" --parser "cat" --output none
