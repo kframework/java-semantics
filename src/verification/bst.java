9651de02421b8c0be5c86bb9f140b4bedadc24d8
laurayuwen <laurayuwen@gmail.com>
2015-06-06 18:38:49 -0500

    avl verification

--- src/verification/bst.java
+++ src/verification/bst.java
@@ -4,14 +4,14 @@ bst left;
 bst right;
 
   public static void main(String[] args) {
-  bst node1 = new bst ();
-bst node2 = new bst ();
-bst node3 = new bst ();
+  bst node1 = new bst (0);
+bst node2 = new bst (0);
+bst node3 = new bst (0);
 node1.left = node2;
 node1.right = node3;
 }
 
-static int find(int v, bst t)
+static boolean find(int v, bst t)
 {
   if (t == null)
     return false;
@@ -23,4 +23,64 @@ static int find(int v, bst t)
     return find(v, t.right);
 }
 
+    
+
+     bst (int value)
+    {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    static bst insert(int v, bst t)
+    {
+        if (t == null)
+            return new bst(v);
+
+        if (v < t.value)
+            t.left = insert(v, t.left);
+        else if (v > t.value)
+            t.right = insert(v, t.right);
+
+        return t;
+    }
+
+
+    static int find_min(bst t)
+    {
+        if (t.left == null)
+            return t.value;
+        else
+            return find_min(t.left);
+    }
+
+    static bst delete(int v, bst t)
+    {
+        int min;
+
+        if (t == null)
+            return t;
+
+        if (v == t.value) {
+            if (t.left == null) {
+                                return t.right;
+            }
+            else if (t.right == null) {
+
+                return t.left;
+            }
+            else {
+                min = find_min(t.right);
+                t.right = delete(min, t.right);
+                t.value = min;
+            }
+        }
+        else if (v < t.value)
+            t.left = delete(v, t.left);
+        else
+            t.right = delete(v, t.right);
+
+        return t;
+    }
 }
